7b1a017a5e1698ba989efbe736e0315f8d570168
Starfolll <andrey.kovyarov@hotmail.com>
2019-12-15 22:39:12 +0200

    hotfix

--- scripts/build/scripts/consoleLoger/logDash.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function logDash() {
-    console.log(" | | |" + "-".repeat(15) + "| | |");
-}
-exports.default = logDash;
