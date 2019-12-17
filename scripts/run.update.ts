d41d741c359cee979c126e1f06127e7afed0c096
Starfolll <andrey.kovyarov@hotmail.com>
2019-12-17 21:11:34 +0200

    hotfix

--- scripts/run.update.ts
+++ scripts/run.update.ts
@@ -5,7 +5,8 @@ import chalk from "chalk";
 export async function runUpdate() {
     const rl = readline.createInterface({
         input: process.stdin,
-        output: process.stdout
+        output: process.stdout,
+        prompt: "> "
     });
 
     console.log(` | ${chalk.greenBright("Write commit name")}`);
@@ -15,7 +16,7 @@ export async function runUpdate() {
         const addStdout = execSync(`git add .`);
         console.log(addStdout + "\n");
 
-        const commitStdout = execSync(`git commit -m ${line}`);
+        const commitStdout = execSync(`git commit -m "${line}"`);
         console.log(commitStdout + "\n");
 
         const pushStdout = execSync("git push");
