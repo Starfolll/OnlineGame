3b37ae20155dc7bee56ee9d9e8d64c31e7fcd5f5
Starfolll <andrey.kovyarov@hotmail.com>
2019-12-17 21:10:07 +0200

    update run added

---
+++ scripts/run.update.ts
@@ -0,0 +1,25 @@
+import {execSync} from "child_process";
+import readline from "readline";
+import chalk from "chalk";
+
+export async function runUpdate() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    console.log(` | ${chalk.greenBright("Write commit name")}`);
+    for await (const line of rl) {
+        console.log();
+
+        const addStdout = execSync(`git add .`);
+        console.log(addStdout + "\n");
+
+        const commitStdout = execSync(`git commit -m ${line}`);
+        console.log(commitStdout + "\n");
+
+        const pushStdout = execSync("git push");
+        console.log(pushStdout + "\n");
+        break;
+    }
+}
--- server.ts
+++ server.ts
@@ -1,10 +1,17 @@
 import {runDevelopmentBuild} from "./scripts/run.dev";
 import {runProductionBuild} from "./scripts/run.prod";
+import {runUpdate} from "./scripts/run.update";
 
 (async () => {
     const webPort: number = +process.env.npm_package_config_webPort!;
     const gameWSPort: number = +process.env.npm_package_config_gameWSPort!;
-    const isDev: any = process.argv.some(arg => arg === "--dev");
+    const isDev: boolean = process.argv.some(arg => arg === "--dev");
+    const update: boolean = process.argv.some(arg => arg === "--update");
+
+    if (update) {
+        await runUpdate();
+        return;
+    }
 
     if (isDev) await runDevelopmentBuild(webPort, gameWSPort);
     else await runProductionBuild(webPort, gameWSPort);
